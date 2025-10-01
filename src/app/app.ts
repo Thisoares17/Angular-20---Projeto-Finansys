49086e5b74a9e5f319e8d9e8c8bd90c0848fc024
Thiago Soares <thiago_soares2@hotmail.com>
2025-10-01 00:32:01 -0300

    add navBar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CategoriesRoutingModule } from "./pages/categories/categories-routing-module";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CategoriesRoutingModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
